b7c4d7084329691547cee647062222daf47e1293
adnan chowdhury <adnan.chowdhury@vanta.com>
2025-02-28 11:42:43 -0600

    init

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp bash_profile ~/.bash_profile
+cp tmux.conf ~/.tmux.conf
+
+# vim
+# install Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/{_tmp,backup,colors}
+cp vimrc ~/.vimrc
+cp sierra ~/.vim/colors/sierra.vim
+vim +PluginInstall +qall
