a7283507320926502d1961e535e372e44b89e50a
adnan chowdhury <adnan.chowdhury@vanta.com>
2026-04-17 19:26:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -67,3 +67,9 @@ cat > ~/.config/nvim/coc-settings.json << 'EOF'
 EOF
 
 echo "✓ Created ~/.config/nvim/coc-settings.json"
+
+# install glow for reading markdown
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
